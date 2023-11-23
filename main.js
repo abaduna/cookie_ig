e2f127522991641ef72eab7817d456bf2f6945f6
abaduna <64230830+abaduna@users.noreply.github.com>
2023-11-23 14:56:07 -0300

    localStorague

--- main.js
+++ main.js
@@ -7,6 +7,27 @@ var ano = fecha.getFullYear();
 var fechacompleta = dia+"/"+mes+"/"+ano;
 var objetivo = document.getElementById('texto_nav1');
 objetivo.innerHTML = fechacompleta;
+let localStorage_ig = Window.localStorage
+console.log(localStorage_ig);
+localStorage.setItem("miGato", "Juan");
+let mostrarEnPantalla = localStorage.getItem("miGato");
+console.log(mostrarEnPantalla);
+let mostrarEnPantalla_id = localStorage.getItem("miGato");
+
+var cantidadElementos = localStorage.length;
+
+for (var i = 0; i < cantidadElementos; i++) {
+    var clave = localStorage.key(i);
+    var valor = localStorage.getItem(clave);
+
+    let localStorage_text = "Clave: " + clave + ", Valor: " + valor
+    alert(localStorage_text)
+}
+
+
+
+
+
 function leerCookie(nombre_cookie) {
     var cookies = document.cookie.split(";");
     for (var i = 0; i < cookies.length; i++) {
